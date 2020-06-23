7610d29bb2f78e1354c709b5762fddae64a127bd
Pablo Garza <pgarza917@fb.com>
2020-06-23 19:27:14 -0400

    Comments for member variables and list

--- app/src/main/java/com/example/simpletodo/MainActivity.java
+++ app/src/main/java/com/example/simpletodo/MainActivity.java
@@ -23,12 +23,15 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        // Member variables for each view added in the design of the app
         btnAdd = findViewById(R.id.btnAdd);
         edItem = findViewById(R.id.etItem);
         rvItems = findViewById(R.id.rvItems);
 
+        // items will be a model for our data
         items = new ArrayList<>();
 
+        // Mocking data in items list to start
         items.add("Buy juice");
         items.add("Go for a run");
         items.add("Have fun!");
