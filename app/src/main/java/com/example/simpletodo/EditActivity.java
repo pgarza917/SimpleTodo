1266d7e7d9c286d7299539b4f0ee65db9d936e46
Pablo Garza <pgarza917@fb.com>
2020-06-24 03:00:48 -0400

    Added logic to save button to handle saving edits and flowing from Edit to Main Activity

--- app/src/main/java/com/example/simpletodo/EditActivity.java
+++ app/src/main/java/com/example/simpletodo/EditActivity.java
@@ -2,7 +2,9 @@ package com.example.simpletodo;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
@@ -23,5 +25,29 @@ public class EditActivity extends AppCompatActivity {
 
         // Making activity title appearing to user more descriptive
         getSupportActionBar().setTitle("Edit Item");
+
+        // Pre-populate the edit text view with data received from intent
+        etItem.setText(getIntent().getStringExtra(MainActivity.KEY_ITEM_TEXT));
+
+        // Adds listener to button to handle clicks
+        btnSave.setOnClickListener(new View.OnClickListener() {
+            @Override
+            // When the user is done editing, they click the save button
+            public void onClick(View view) {
+                // Create intent to contain user modifications (using intent as shell, not
+                // launching new activity so no parameters needed)
+                Intent intent = new Intent();
+
+                // Pass the data (user edits)
+                intent.putExtra(MainActivity.KEY_ITEM_TEXT, etItem.getText().toString());
+                intent.putExtra(MainActivity.KEY_ITEM_POSITION, getIntent().getExtras().getInt(MainActivity.KEY_ITEM_POSITION));
+
+                // Set the result of the intent
+                setResult(RESULT_OK, intent);
+
+                // Finish activity, close screen, and go back
+                finish();
+            }
+        });
     }
 }
